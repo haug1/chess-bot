ce625563ad8198adc5d005011314f2d28e0ebe07
Lars-Ragnar A. Haugen <larsiah@hotmail.com>
2022-10-13 00:46:07 +0200

    fix: catch errors and return 500

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,5 @@
 import createFastify from "fastify";
+import { error } from "./logger.js";
 import {
   getBestMoveBasedOnFEN,
   getBestMoveBasedOnMoves,
@@ -27,18 +28,23 @@ export const fastify = createFastify({
 // Returns text/event-stream
 // each chunk is a stockfish evaluation,
 // see `stockfish/parser.js` for structure
-fastify.post("/moves/sse", (request, reply) => {
+fastify.post("/moves/sse", async (request, reply) => {
   reply.raw.writeHead(200, {
     "Content-Type": "text/event-stream",
     Connection: "keep-alive",
     "Cache-Control": "no-cache,no-transform",
     "x-no-compression": 1,
   });
-  getBestMoveBasedOnMoves(
-    request.body.moves,
-    (v) => reply.raw.write(`data: ${JSON.stringify(v)}\n\n`),
-    () => reply.raw.end()
-  );
+  try {
+    await getBestMoveBasedOnMoves(
+      request.body.moves,
+      (v) => reply.raw.write(`data: ${JSON.stringify(v)}\n\n`),
+      () => reply.raw.end()
+    );
+  } catch (e) {
+    error(e);
+    reply.code(500).send({ message: e.message });
+  }
 });
 
 // Sample request:
@@ -48,18 +54,23 @@ fastify.post("/moves/sse", (request, reply) => {
 // Returns text/event-stream
 // each chunk is a stockfish evaluation,
 // see `stockfish/parser.js` for structure
-fastify.post("/fen/sse", (request, reply) => {
+fastify.post("/fen/sse", async (request, reply) => {
   reply.raw.writeHead(200, {
     "Content-Type": "text/event-stream",
     Connection: "keep-alive",
     "Cache-Control": "no-cache,no-transform",
     "x-no-compression": 1,
   });
-  getBestMoveBasedOnFEN(
-    request.body,
-    (v) => reply.raw.write(`data: ${JSON.stringify(v)}\n\n`),
-    () => reply.raw.end()
-  );
+  try {
+    await getBestMoveBasedOnFEN(
+      request.body,
+      (v) => reply.raw.write(`data: ${JSON.stringify(v)}\n\n`),
+      () => reply.raw.end()
+    );
+  } catch (e) {
+    error(e);
+    reply.code(500).send({ message: e.message });
+  }
 });
 
 // Sample request:
@@ -67,18 +78,28 @@ fastify.post("/fen/sse", (request, reply) => {
 // Content-Type: application/json
 // { moves: ["e4"] }
 // Returns only the final bestmove evaluation (which can take a long time sometimes)
-fastify.post("/moves", async (request, reply) =>
-  reply.send(await getBestMoveBasedOnMoves(request.body.moves))
-);
+fastify.post("/moves", async (request, reply) => {
+  try {
+    reply.send(await getBestMoveBasedOnMoves(request.body.moves));
+  } catch (e) {
+    error(e);
+    reply.code(500).send({ message: e.message });
+  }
+});
 
 // Sample request:
 // POST /fen
 // Content-Type: text/plain
 // rnbqkbnr/pp1ppppp/8/2p5/8/3P4/PPP1PPPP/RNBQKBNR w KQkq c6 0 2
 // Returns only the final bestmove evaluation (which can take a long time sometimes)
-fastify.post("/fen", async (request, reply) =>
-  reply.send(await getBestMoveBasedOnFEN(request.body))
-);
+fastify.post("/fen", async (request, reply) => {
+  try {
+    reply.send(await getBestMoveBasedOnFEN(request.body));
+  } catch (e) {
+    error(e);
+    reply.code(500).send({ message: e.message });
+  }
+});
 
 export async function startServer() {
   if (fastify.server?.listening) return;
