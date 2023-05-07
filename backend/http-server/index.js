79c9d71271680dd542c4bd0bf0ad2d35c2c87366
lars <larsiah@hotmail.com>
2023-05-07 16:59:56 +0200

    feat(backend): support for specifying \host\ and \port\ as startup arguments

--- backend/http-server/index.js
+++ backend/http-server/index.js
@@ -1,11 +1,30 @@
+import yargs from 'yargs'
 import { startEngine } from '../src/index.js'
 import { fastify } from './config.js'
 import './logger.js'
+
+const { argv } = yargs(process.argv)
+  .option('host', {
+    alias: 'h',
+    describe: 'Host to bind to',
+    type: 'string',
+    default: '0.0.0.0',
+  })
+  .option('port', {
+    alias: 'p',
+    describe: 'Port to bind to',
+    type: 'number',
+    default: 8080,
+  })
+
 ;(async function main() {
   if (fastify.server?.listening) return
   try {
     await startEngine()
-    return fastify.listen({ port: process.env.PORT || 8080 })
+    return fastify.listen({
+      host: argv.host,
+      port: argv.port,
+    })
   } catch (err) {
     console.error(err)
     process.exit(1)
